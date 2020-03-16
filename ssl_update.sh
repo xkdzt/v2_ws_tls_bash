c26075b22791d9a8c521cff84e9b0a77484acfd9
xkdzt <60614617+xkdzt@users.noreply.github.com>
2020-03-16 00:53:49 -0700

    Add files via upload

---
+++ ssl_update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+v2ray_qr_config_file="/usr/local/vmess_qr.json"
+domain=$(grep '\"add\"' $v2ray_qr_config_file | awk -F '"' '{print $4}')
+
+systemctl stop nginx &> /dev/null
+sleep 1
+"/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /dev/null
+"/root/.acme.sh"/acme.sh --installcert -d ${domain} --fullchainpath /home/v2ray.crt --keypath /home/v2ray.key --ecc
+sleep1
+openssl x509 -in /home/v2ray.crt -out /home/v2ray.pem
+sleep 1
+systemctl start nginx &> /dev/null
